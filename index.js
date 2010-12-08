cbc234493518d829178c48df546a09d4a31fa8e0
ranzwertig <git@christianranz.com>
2010-12-08 15:26:48 +0100

    adfixed redirection, added fileHandler to provide static files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var router = require("./lib/node-http-route");
 
-router.get("/test/", function(req, res) {
-    res.sendText(200, "test");
-});
+router.get("/test/file/", router.fileHandler("index.js"));
 
 router.get("/test/foo/", function(req, res) {
     res.sendText(200, "foo");
+});
+
+router.get("/test/", function(req, res) {
+    res.sendText(200, "test");
 });
 
 router.listen(8080, "127.0.0.1");
